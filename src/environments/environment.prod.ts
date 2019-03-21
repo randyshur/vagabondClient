b43a4230eac21ba3053e5dd4b149c05c4d818c62
Maddie Ijams <maddieijams@gmail.com>
2019-03-21 09:49:03 -0400

    idk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ switch (window.location.hostname) {
     // this is the deployed angular application
     case 'https://vagabond-client-mi.herokuapp.com/':
         // this is the full url of your deployed API
-        APIURL = 'https://shur-vagabond-server.herokuapp.com/'
+        APIURL = 'http://localhost:4050'
         break;
     default:
         // this is the local host name of your API
