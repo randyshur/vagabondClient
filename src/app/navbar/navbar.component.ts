5366162eb0d6118f3adf48a830d45e0c003c9a1e
randyshur <rjdev@shurvideo.com>
2019-03-22 12:24:54 -0400

    temporarily add user to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { UserService } from '../services/user.service';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  token: any;
+  username: any;
+  user: any;
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService, private http: HttpClientModule
+  ) {
+    // redirect to home if already logged in
+    //if (this.authenticationService.currentUserValue) {
+    //    this.router.navigate(['/']);
+    //}
+  }
 
   ngOnInit() {
+    this.setToken()
   }
+
+  setToken() {
+    const token = localStorage.getItem('token')
+    this.token = token;
+
+    this.userService.getUser().subscribe(data => {
+      console.log(data)
+      this.user = data
+      console.log(this.user.username)
+    }
+    )
+  }
+
+  //const user = 'bubba';
+  //this.username = user;
 
 }
 
